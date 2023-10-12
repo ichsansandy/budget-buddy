ddb50e664ea023b26667f27c711a93aa3cbab827
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-12 19:56:20 +0700

    Styling layout and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'hello#index'
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    sign_up: 'register'
+  }
+
+  authenticated :user do
+    root 'categories#home', as: :authenticated_root
+  end
+  
+  unauthenticated do
+    root 'categories#index', as: :unauthenticated_root
+  end
+
+  resources :categories, only: %i[index new create destroy] do
+    resources :transactions, only: %i[index new create]
+  end
 end

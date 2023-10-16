f6768661a2e770733e2e6d429167f5c4d5193e95
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-10-16 18:26:23 +0700

    Remove tailwind plugin in config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,12 +1,9 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
   content: [
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
@@ -21,11 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-    require('flowbite/plugin'),
-  ],
+  plugins: [],
 };
